4e13a272748d7ddc7981c89e162ec96e345850f7
Daniel dos Santos Carril <dancarril22@gmail.com>
2022-01-13 18:50:48 +0100

    start_game() by Daniel

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -14,6 +14,16 @@ class Director:
 
     def start_game(self):
         """
-        comments
-        
+        This method handles the settings to start the game running. 
+        It´s called by the __main__ file. 
+        It calls card class and its methods.
         """
+        print (self.card)
+        while self.is_playing:
+            self.card.flip()
+            self.card.updates()
+            #here comes a play_again method() or
+        #a print("play_again") maybe
+            
+            
+
