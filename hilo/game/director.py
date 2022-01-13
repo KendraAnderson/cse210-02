c7f5e3b766e113adf4379ad986664f821cebdfd5
jessbarzee <bar19007@byui.edu>
2022-01-13 15:58:12 -0700

    input changes

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -17,11 +17,23 @@ class Director:
         comments
         
         """
-    def get_inputs(self):
-        """Ask the user if they want to roll.
+
+    def get_guess(self):
+        """Ask the user if they guess h or l
 
         Args:
             self (Director): An instance of Director.
             """
-        flip_card = input("Flip a card [y/n] ")
-        self.is_playing = (flip_card == "y")
+        h_or_l = input("Higher or Lower? [h/l]")
+
+
+    def play_again(self):
+        """Ask the user if they want to play again
+
+        Args:
+            self (Director): An instance of Director.
+            """
+
+
+        card = input("Want to play again? [y/n] ")
+        self.is_playing = (card == "y")
