f320c6ca7281210508dba218065d10b0b11d1523
Kendra Anderson <kor04003@byui.edu>
2022-01-14 18:51:22 -0800

    comments fix

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -3,21 +3,23 @@ import random
 
 class Card:
     """A card with a number 1-13 on the back.
-    The responsibility of card is to keep track of which numbered card
-    is face up and calculate whether the players guess was correct. It then 
-    gives them points for correct guesses or takes them away for incorrect guesses.
+
+    The responsibility of card is to keep track of which number
+    is face up on the card.
+
     Attributes:
-        value(int): The number on the card currently facing up.    
+        value (int): The number on the card currently facing up.    
     """
     def __init__(self):
         """Contructs a new instance of Card.
+        
         Args:
             self (Card): An instance of Card.        
         """
         self.value = 0
 
     def flip(self):
-        """Generates a new random value and calculates the points for the card.
+        """Generates a new random value for the card.
         
         Args:
             self (Card): An instance of Card.
